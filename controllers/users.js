500041f3995c0a2cd6d493ad2d6349ab44c4f934
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-03-30 13:14:14 +0300

    апдейт профиля

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,32 @@
-const User = require('../models/users');
+const User = require('../models/user');
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => res.status(500).send({ message: err }));
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => res.status(500).send({ message: err }));
 };
 
 module.exports.getUser = (req, res) => {
   User.find(req.params.id)
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Error' }));
+    .catch((err) => res.status(500).send({ message: err }));
+};
+
+module.exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(req.user._id, { name: req.body.name, about: req.body.about })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar })
+    .then((avatar) => res.send({ data: avatar }))
+    .catch((err) => res.send({ message: err }));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-const { createUser, getUsers } = require('../controllers/users');
+const {
+  createUser, getUsers, updateUser, updateAvatar,
+} = require('../controllers/users');
 
 router.post('/users', createUser);
 router.get('/users', getUsers);
+router.patch('/users/me', updateUser);
+router.patch('/users/me/avatar', updateAvatar);
 module.exports = router;
