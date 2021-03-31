f49cd5f78c1c171493ba31a849737e9d132cbf19
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-03-31 19:33:28 +0300

    ошибки

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,9 +5,12 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: 'Неверные данные' });
+      } else res.status(500).send({ message: err.message });
+    });
 };
-
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
@@ -15,7 +18,7 @@ module.exports.getCards = (req, res) => {
 };
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send({ data: card }))
+    .then(() => res.send({ message: 'Пост удален' }))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
         res.status(400).send({ message: 'Неверные данные' });
