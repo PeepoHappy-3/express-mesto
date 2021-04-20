07488fa1f07ca38cec577c3c3c4c217cdf4b77c1
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-04-21 00:10:10 +0300

    1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,18 +5,17 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  //if (!authorization || !authorization.startsWith('Bearer ')) {
+   // next(new UnauthorizedError('Необходима авторизация'));
+// } else {
+  let payload;
+  const token = req.cookies.jwt;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
-  } else {
-    let payload;
-    const token = req.cookies.jwt;
-    console.log(req);
-    try {
-      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-    } catch (err) {
-      next(new UnauthorizedError('Необходима авторизация'));
-    }
-    req.user = payload;
-    next();
   }
+  req.user = payload;
+  next();
+ // }
 };
