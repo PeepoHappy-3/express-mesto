fc493693211ce43cbc5e0ae6ffcda07e540ea8b0
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-04-20 22:22:36 +0300

    1

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(requestLogger);
-app.use(cors());
+app.use(cors(corsOptions));
 app.post('/signin', validateUserLogin, login);
 app.post('/signup', validateUserCreate, createUser);
 app.use(cookieParser());
