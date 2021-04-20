74807f51417aa7010ffac65cf2d8520350b0ed10
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-04-20 22:03:27 +0300

    1

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ app.use(bodyParser.json());
 app.use(requestLogger);
 app.use((req, res, next) => {
   const { origin } = req.headers;
-  res.header('Access-Control-Allow-Origin', origin);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Credentials', true);
