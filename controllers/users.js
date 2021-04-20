d4fb1d98c9027c4b8fd246a411ff997566731a1d
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-04-20 21:15:03 +0300

    1

--- controllers/users.js
+++ controllers/users.js
@@ -108,7 +108,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then(user => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: 3600000 * 24 * 7 });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true })
         .end();
     }).catch(err => {
       next(new UnauthorizedError(err.message));
